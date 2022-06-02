a965400df0d5c05e9c9708072e24dbfbe4b105b0
ovepfunkep <90245284+ovepfunkep@users.noreply.github.com>
2022-06-02 09:54:55 +0700

    Add files via upload

---
+++ CheeseMakingApp/CheeseMakingApp/AccountantForm.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static CheeseMakingApp.Utilities;
+
+namespace CheeseMakingApp
+{
+    public partial class AccountantForm : Form
+    {
+        public AccountantForm(LoginForm loginFormGiven)
+        {
+            InitializeComponent();
+            loginFormTaken = loginFormGiven;
+        }
+        
+        private LoginForm loginFormTaken;
+
+        private void purchasesBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.purchasesBindingNavigator.BindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.cheeseMakingDataSet);
+        }
+
+        private void Accountant_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "cheeseMakingDataSet.Supliers". При необходимости она может быть перемещена или удалена.
+            this.supliersTableAdapter.Fill(this.cheeseMakingDataSet.Supliers);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "cheeseMakingDataSet.Locations". При необходимости она может быть перемещена или удалена.
+            this.locationsTableAdapter.Fill(this.cheeseMakingDataSet.Locations);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "cheeseMakingDataSet.Customers". При необходимости она может быть перемещена или удалена.
+            this.customersTableAdapter.Fill(this.cheeseMakingDataSet.Customers);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "cheeseMakingDataSet.Sales". При необходимости она может быть перемещена или удалена.
+            this.salesTableAdapter.Fill(this.cheeseMakingDataSet.Sales);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "cheeseMakingDataSet.Purchases". При необходимости она может быть перемещена или удалена.
+            this.purchasesTableAdapter.Fill(this.cheeseMakingDataSet.Purchases);
+
+        }
+
+        private void AccountantForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            loginFormTaken.Show();
+        }
+
+        private void buttonOpenTableSales_Click(object sender, EventArgs e)
+        {
+            TableForm tableForm = new TableForm();
+            fillDataGrid("Select Sales.Id, Customers._name, Locations._name, Cost from Sales inner join Customers on Customers.Id = Sales.CustomerId inner join Locations on Locations.Id = Sales.StorageId", tableForm.dataGridView1);
+            tableForm.dataGridView1.Columns[1].HeaderText = "Customer";
+            tableForm.dataGridView1.Columns[2].HeaderText = "Storage";
+            tableForm.dataGridView1.Columns.Add("columnButtons", "Click 2 see sorts");
+            tableForm.Text = "Sales";
+            tableForm.Show();
+        }
+
+        private void panelSales_MouseEnter(object sender, EventArgs e)
+        {
+            purchasesBindingNavigator.BindingSource = salesBindingSource;
+        }
+
+        private void panelPurchases_MouseEnter(object sender, EventArgs e)
+        {
+            purchasesBindingNavigator.BindingSource = purchasesBindingSource;
+        }
+
+        private void buttonOpenTablePurchases_Click(object sender, EventArgs e)
+        {
+            TableForm tableForm = new TableForm();
+            fillDataGrid("Select Purchases.Id, Supliers._name, DateOf, Cost from Purchases inner join Supliers on Supliers.Id = SuplierId", tableForm.dataGridView1);
+            tableForm.dataGridView1.Columns[1].HeaderText = "Customer";
+            tableForm.dataGridView1.Columns.Add("columnButtons", "Click 2 see ingredients");
+            tableForm.Text = "Purchases";
+            tableForm.Show();
+        }
+    }
+}
---
+++ CheeseMakingApp/CheeseMakingApp/Utilities.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Data;
+
+namespace CheeseMakingApp
+{
+    class Utilities
+    {
+        public static string connectionString = @"Data Source=Dbsrv\sql2021;Initial Catalog=CheeseMaking;Integrated Security=True";
+        
+        public static bool checkUser(string username, string password)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "Select Login from Accounts where Login = @Username and Password = @Password";
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("Username", username);
+                command.Parameters.AddWithValue("Password", password);
+                SqlDataReader reader = command.ExecuteReader();
+                return reader.Read();
+            }
+        }
+
+        public static bool isUserAdmin(string username)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "Select JobPositions._name from Accounts inner join StaffAccounts on Accounts.Id = StaffAccounts.AccountId inner join Staff on StaffAccounts.StaffId = Staff.Id inner join StaffPositions on Staff.Id = StaffPositions.StaffId inner join JobPositions on JobPositions.Id = StaffPositions.PositionId where Accounts.Login = @username";
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("username", username);
+                SqlDataReader reader = command.ExecuteReader();
+                reader.Read();
+                if (reader["_name"].ToString() == "Director") return true;
+                else return false;
+            }
+        }
+
+        public static bool isUserAccountant(string username)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "Select JobPositions._name from Accounts inner join StaffAccounts on Accounts.Id = StaffAccounts.AccountId inner join Staff on StaffAccounts.StaffId = Staff.Id inner join StaffPositions on Staff.Id = StaffPositions.StaffId inner join JobPositions on JobPositions.Id = StaffPositions.PositionId where Accounts.Login = @username";
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("username", username);
+                SqlDataReader reader = command.ExecuteReader();
+                reader.Read();
+                if (reader["_name"].ToString() == "Accountent") return true;
+                else return false;
+            }
+        }
+
+        public static DataSet fillDataGrid(string query, DataGridView dataGrid)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                DataSet dataSet = new DataSet();
+                adapter.Fill(dataSet);
+                dataGrid.DataSource = dataSet.Tables[0];
+                return dataSet;
+            }
+        }
+
+        public static string checkNextStep(int batchId)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "Select Id,_name from Processes";
+                connection.Open();
+                SqlDataAdapter adapter1 = new SqlDataAdapter(query, connection);
+                DataSet dataset = new DataSet();
+                dataset.Tables.Add();
+                adapter1.Fill(dataset.Tables[0]);
+                query = "Select Processes.Id from Processes inner join Activity on Activity.ProcessId = Processes.Id where Activity.BatchId = @batchId";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("batchId", batchId);
+                SqlDataAdapter adapter2 = new SqlDataAdapter(command);
+                dataset.Tables.Add();
+                adapter2.Fill(dataset.Tables[1]);
+                return dataset.Tables[0].Rows[dataset.Tables[1].Rows.Count][1].ToString();
+            }
+        }
+
+        public static object[][] getData(SqlCommand command)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                command.Connection = connection;
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataSet dataSet = new DataSet();
+                adapter.Fill(dataSet);
+                object[][] array = new object[dataSet.Tables[0].Rows.Count][];
+                foreach (DataRow row in dataSet.Tables[0].Rows)
+                {
+                    array[dataSet.Tables[0].Rows.IndexOf(row)] = new object[row.ItemArray.Length];
+                    row.ItemArray.CopyTo(array[dataSet.Tables[0].Rows.IndexOf(row)],0);
+                }
+                return array;
+            }
+        }
+    }
+}
